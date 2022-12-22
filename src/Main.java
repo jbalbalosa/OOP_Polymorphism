668c22b1c64d87178c0691ad0f25a75eafbc62c9
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-22 16:11:57 +0900

    commit

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Udemy: Java Programming Masterclass");
-        System.out.printf("OOP Polymorphism");
-        System.out.printf("------------------------------------");
-
-
-    }
-}
